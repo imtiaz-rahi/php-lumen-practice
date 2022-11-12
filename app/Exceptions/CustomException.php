48e4fb1b7feaf7250113bd79eefd826ba3ea7da4
Imtiaz Rahi <imtiaz.rahi@gmail.com>
2022-11-13 01:58:39 +0600

    Added sample of custom exception

--- app/Exceptions/CustomException.php
+++ app/Exceptions/CustomException.php
@@ -2,8 +2,24 @@
 
 namespace App\Exceptions;
 
-// TODO implement this
-class CustomException
+/**
+ * Class CustomException.
+ *
+ * @author Imtiaz Rahi
+ * @since 2022-11-13
+ * @see https://www.php.net/manual/en/language.exceptions.extending.php
+ * @see https://www.educba.com/php-custom-exception/
+ */
+class CustomException extends \Exception
 {
 
+    public function __construct($message, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
 }
